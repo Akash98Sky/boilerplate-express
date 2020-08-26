f8400716343137d441ca8a440844114fdfb5ab4a
Akash Mondal <a98mondal@gmail.com>
2020-08-26 12:37:41 +0000

    completed basic node and express challenge

--- myApp.js
+++ myApp.js
@@ -3,10 +3,14 @@ var express = require('express');
 var app = express();
 
 // --> 7)  Mount the Logger middleware here
-
+app.use(function(req, res, next) {
+	console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+	next();
+});
 
 // --> 11)  Mount the body-parser middleware  here
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /** 1) Meet the node console. */
 console.log("Hello World");
@@ -41,13 +45,30 @@ process.env.MESSAGE_STYLE = "uppercase"
 
 
 /** 8) Chaining middleware. A Time server */
+app.get('/now', (req, res, next) => {
+	req.time = new Date().toString();
+	next();
+}, (req, res) => {
+	res.send({ time: req.time });
+});
 
 
 /** 9)  Get input from client - Route parameters */
+app.get('/:word/echo', (req, res) => {
+	res.send({ echo: req.params.word });
+});
 
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
+/**
+ * @param {Requests} req
+ * @param {Response} res
+ */
+const nameHandler = (req, res) => {
+	res.send({ name: req.query.first + ' ' + req.query.last });
+};
+app.route('/name').get(nameHandler)//.post(nameHandler);
 
 
 /** 11) Get ready for POST Requests - the `body-parser` */
@@ -55,7 +76,9 @@ process.env.MESSAGE_STYLE = "uppercase"
 
 
 /** 12) Get data form POST  */
-
+app.post('/name', (req, res) => {
+	res.send({ name: req.body.first + ' ' + req.body.last });
+});
 
 
 // This would be part of the basic setup of an Express app
